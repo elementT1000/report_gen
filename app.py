b84533fe18a223a77f3935db1ba253a92442c7d2
Ethan Trotter <trotter1009@gmail.com>
2023-02-13 07:49:33 -0800

    Add .csv upload and a function for processing json dataframes.

--- app.py
+++ app.py
@@ -19,20 +19,26 @@ from statsmodels.sandbox.regression.predstd import wls_prediction_std
 
 from gait_slicer import *
 from table_maker import *
+from data_modifications import process_df
 from normalized_gait import highlight_phase_median
+
+import base64
+import io
+
+from dash.exceptions import PreventUpdate
 
 
 ####################################
 #Temp Table Info
 ####################################
-#Constants for graph function
-################
+#TODO: Initialize the page without any data
 csv_name = "Dataset_1_Ethan_01062023.csv"
-#pln = "Sagittal Plane Right"
+#TODO: The current 'slice_df_into_phases()' uses the same system despite the leg. I need to set up a dictionary to switch the system based on the leg selected.
+#For the af and pf planes, I need a toggle to select which leg. For the sagittal planes, I just need to switch it based on which side is collected. 
+#This is important for the tables
 system = "RL - RunLab"
 ################
 
-df = pd.read_csv(csv_name, index_col=0, header=[0,1])
 
 #Dash Application Starts
 app = dash.Dash(__name__,
@@ -63,6 +69,24 @@ OPTIONS = dbc.Card(
             [
                 html.H3("Options", style={"color": "#ffffff"}),
                 html.Hr(),
+
+                dcc.Upload(
+                    id='upload-data',
+                    children=['Upload Movement Data'],
+                    style={
+                        'width': '100%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin': '0px'
+                    },
+                ),
+                dcc.Store(id='store'),
+                html.Div(id='csv-name', style={"color": "#ffffff"}),
+
                 dbc.Label("Body Plane:", style={"color": "#ffffff"}),
                 dcc.Dropdown(
                     id='plane-radio',
@@ -70,16 +94,24 @@ OPTIONS = dbc.Card(
                     ['Anterior Frontal Plane', 'Sagittal Plane Right', 'Sagittal Plane Left', 'Posterior Frontal Plane'],
                     value='Sagittal Plane Right',
                     clearable=False,
-                    style={'color': 'black'}
+                    style={
+                        'color': 'black',
+                        'margin': '0px'
+                    }
                 ),
+
                 dbc.Label("Joint Selection:", style={"color": "#ffffff"}),
                 dcc.Dropdown(
                     id='joint-radio',
                     #options= ['RightArm', 'RightHip', 'RightKnee', 'RightAnkle', 'RightToe'],
                     #value='RightKnee',
                     clearable=False,
-                    style={'color': 'black'}
+                    style={
+                        'color': 'black',
+                        'margin': '0px'
+                    }
                 ),
+
                 dbc.Label("Phase Highlight:", style={"color": "#ffffff"}),
                 dcc.Dropdown(
                     id='phase-highlight',
@@ -88,7 +120,10 @@ OPTIONS = dbc.Card(
                     'Initial Swing', 'Midswing', 'Terminal Swing'],
                     value='None',
                     clearable=False,
-                    style={'color': 'primary'}
+                    style={
+                        'color': 'primary',
+                        'margin': '0px'
+                    }
                 )
             ],
         )
@@ -130,7 +165,7 @@ app.layout = html.Div(
                 dbc.Row(
                     #Here, I need to add in the columns if I want them to be printed
                     [  #May need a card group here if I want to control the height
-                        dbc.Col(OPTIONS, width=2, style={'height': '100%'}),
+                        dbc.Col(OPTIONS, width=3, style={'height': '100%'}),
                         dbc.Col([
                             dcc.Graph(id='graph'),  
                         ], width=9)
@@ -153,7 +188,7 @@ app.layout = html.Div(
                 ),
             ], 
             
-            style={"maxWidth": "1150px"} #For A4 style .pdf files, 900px seems to be the max
+            style={"maxWidth": "1300px"} #For A4 style .pdf files, 900px seems to be the max
         ),
         #The button to download goes here if you need it
         
@@ -162,16 +197,42 @@ app.layout = html.Div(
 )
 
 #Functionality
+#This successfully returns the .csv name, so it looks like it's uploaded b/c store doesnt't throw an error
+@app.callback(
+    Output(component_id='csv-name', component_property='children'),
+    Output('store', 'data'),
+    Input('upload-data', 'contents'),
+    State('upload-data', 'filename')
+)
+def store_data(contents, filename):
+    if contents is None:
+        raise PreventUpdate
+
+    _, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+
+    df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
+
+    return filename, df.to_json(date_format='iso', orient='split')
+
 @app.callback(
     Output(component_id='stance-table', component_property='data'),
     Output(component_id='stance-table', component_property='columns'),
     Output(component_id='swing-table', component_property='data'),
     Output(component_id='swing-table', component_property='columns'),
     Input(component_id='plane-radio', component_property='value'),
+    State('store', 'data'),
 )
-def update_table(plane_radio):
+def update_table(plane_radio, upload):
     plane = plane_radio
 
+    if upload is not None:
+        df = process_df(upload)
+    else:
+        df = pd.read_csv(csv_name, index_col=0, header=[0,1])
+        #print("The datatype of the old df is: ")
+        #print(df.iloc[0, 0].dtype)
     #TABLE Operations
     #group list
     gl = slice_df_into_phases(df, plane, system)
@@ -206,8 +267,14 @@ def set_joint_value(available_options):
     Input(component_id='joint-radio', component_property='value'),
     Input(component_id='plane-radio', component_property='value'),
     Input(component_id='phase-highlight', component_property='value'),
+    State('store', 'data'),
 )
-def update_fig(joint_radio, plane_radio, phase_highlight):
+def update_fig(joint_radio, plane_radio, phase_highlight, upload):
+    if upload is not None:
+        df = process_df(upload)
+    else:
+        df = pd.read_csv(csv_name, index_col=0, header=[0,1])
+    
     joint = joint_radio
     pln = plane_radio
     phase = phase_highlight
@@ -280,7 +347,9 @@ def update_fig(joint_radio, plane_radio, phase_highlight):
     fig.update_layout(
         title="Normal Gait Cycle and Deviation", 
         xaxis_title='Percent to Completion', 
-        yaxis_title='Angle')
+        yaxis_title='Angle',
+        showlegend=False
+    )
     #Phase highlight line
     for x in median:
         fig.add_vline(x=x, line_width=2, line_dash="dash", line_color='red')
@@ -323,5 +392,5 @@ app.clientside_callback(
 )'''
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, use_reloader=True)
 
--- table_maker.py
+++ table_maker.py
@@ -11,14 +11,13 @@ def slice_df_into_phases(angle_dataframe: object, plane: str, leg_and_system: st
 
     #Drop the other planes (dff_plane)
     dff_p = dff.loc[:, dff.columns[dff.columns.get_level_values(0).isin([plane, 'Phase'])]]
-
+    
     #Drop the other phase classifications (dff_classified)
     dff_c = dff_p.loc[
         :, dff_p.columns[
             ~dff_p.columns.get_level_values(0).isin(['Phase']) #keep all of the headers that aren't Phase
             ]
         ].join(dff_p[('Phase',leg_and_system)])#Select the proper label system only and add that back to the filtered dff
-
     #groups = df.groupby(('Phase','RL - RunLab'))
 
     group_list = [g for _, g in dff_c.groupby(('Phase','RL - RunLab'))]
@@ -36,10 +35,14 @@ def calculate_mean_min_max(df_list: list):
         df.columns = df.columns.get_level_values(1)
 
         # Calculate the mean, min, and max for each column
-        mean_df = df.mean().astype(int).astype(str)
-        min_val = df.min().astype(int).astype(str)
-        max_val = df.max().astype(int).astype(str)
-        
+        mean_df = df.mean()
+        mean_df = mean_df.astype(int).astype(str) #some sort of weird type handling is going on from json. need to solve.
+        #print(mean_df[2].dtypes)
+        min_val = df.min() #why the fuck is this a string? It only happens when df is created from json
+        min_val = min_val.astype(int).astype(str)
+        max_val = df.max()
+        max_val = max_val.astype(int).astype(str)
+
         # Concatenate dataframes and rename columns
         phase_header = str(phase.iloc[1,0])
         
@@ -55,7 +58,7 @@ def calculate_mean_min_max(df_list: list):
 
         result = pd.concat([result], axis=1) #keys=[phase_header],
         
-        # Append the result to the list
+        # Append the resgait_sectionult to the list
         results_list.append(result)
 
 # Concatenate the results into a single DataFrame
@@ -65,11 +68,15 @@ def calculate_mean_min_max(df_list: list):
 def gait_section_slicer(fc_df, stance=1):
     stance_section = ['Joint Vertex', 'Initial Strike', 'Loading Response', 'Midstance', 'Terminal Stance', 'Toe Off']
     swing_section = ['Joint Vertex', 'Initial Swing', 'Midswing', 'Terminal Swing']
-
+    
     if stance:
-        fc_df = fc_df[stance_section].reindex(columns=stance_section)
+        #Search the df and continue with the items that are present
+        #Handles the case that a phase is not present
+        check_for_stance = [col for col in stance_section if col in fc_df.columns]
+        fc_df = fc_df[check_for_stance].reindex(columns=stance_section)
     else:
-        fc_df = fc_df[swing_section].reindex(columns=swing_section)
+        check_for_swing = [col for col in swing_section if col in fc_df.columns]
+        fc_df = fc_df[check_for_swing].reindex(columns=swing_section)
 
     return fc_df
 
@@ -96,7 +103,7 @@ def datatable_settings_multiindex(mi_df, flatten_char = '_'):
 if __name__ == "__main__":
     #Constants
     ################
-    csv_name = "Dataset_1_Ethan_01062023.csv"
+    csv_name = "Dataset_2_Tony_01092023.csv"
     joint = 'RightKnee'
     pln = "Sagittal Plane Right"
     system = "RL - RunLab"
@@ -108,7 +115,7 @@ if __name__ == "__main__":
     c_df = calculate_mean_min_max(gl)
     c_df.insert(0, 'Joint Vertex', c_df.index, True)
     c_df = c_df.reset_index(drop=True)
-    #print(c_df)
+    print(c_df)
 
 
     app = Dash(__name__)
@@ -116,7 +123,7 @@ if __name__ == "__main__":
     stance_df = gait_section_slicer(c_df, stance=1)
     stance_columns, stance_data = datatable_settings_multiindex(stance_df)
 
-    swing_df = gait_section_slicer(c_df, stance=0)
+    swing_df = gait_section_slicer(c_df, stance=0) #error, section not in index
     swing_columns, swing_data = datatable_settings_multiindex(swing_df)
 
     app.layout = dbc.Container([
