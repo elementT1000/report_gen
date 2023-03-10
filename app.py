7368284cb67c3e79b99b9e11694ab95014c4c5fe
Ethan Trotter <trotter1009@gmail.com>
2023-03-10 11:36:08 -0600

    Rolled back from graph normalization to raw data and the old phase highlights. Moved some functions to the unuses_functions file.

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from statsmodels.sandbox.regression.predstd import wls_prediction_std
 from gait_slicer import *
 from table_maker import *
 from data_modifications import process_df
-from normalized_gait import highlight_phase_median
+from normalized_gait import og_phase, filter_pln_n_joint
 
 import base64
 import io
@@ -279,74 +279,24 @@ def update_fig(joint_radio, plane_radio, phase_highlight, upload):
     pln = plane_radio
     phase = phase_highlight
     
-    #TODO: Most of the following belongs in a seperate function
-    gcl = slice_df_gait_cycles(df, pln, system)
-    f_df = reindex_to_percent_complete(gcl)
-    median = highlight_phase_median(f_df, key=phase)
+    #Inside df
+    i_df = df
+    f_df = filter_pln_n_joint(i_df, pln, system)
+    median = og_phase(f_df, key=phase)
+    #print(median)
 
     #Trace of all datapoints
-    '''trace = go.Scatter(
+    trace = go.Scatter(
         name="Data", 
         x=f_df.index, 
         y=f_df.loc[:, (pln, joint)], 
-        mode='lines+markers')'''
-
-    '''
-    #Polynomial regression in order to model the general waveform of the data
-    #Fit a 2nd degree polynomial to the data (https://ostwalprasad.github.io/machine-learning/Polynomial-Regression-using-statsmodel.html)
-    '''
-    x = f_df.index.to_numpy()[:,np.newaxis] #Create a numpy array and add a new axis
-    index = x.ravel().argsort() #Sort and reindex the array
-    x = x[index].reshape(-1,1) #Reshape the array. -1 infers the first dimension of the new array based on the size of the original array
-
-    y = f_df.loc[:, (pln, joint)].to_numpy()[:,np.newaxis]
-    y = y[index]#Sort y according to x sorted index
-    y = y.reshape(-1, 1) #(n,1)
-    y = np.nan_to_num(y, 0) #handle the case of nan values
-
-    #Generate polynomial and interaction features in a matrix
-    polynomial_features= PolynomialFeatures(degree=5)
-    #Fit to data, the transform it
-    xp = polynomial_features.fit_transform(x)
-
-    '''
-    Use ordinary least squares for regression
-    Polynomial regression for n degrees. y=b0 + b1x + b2x^2 ...+ bnx^n
-    '''
-    model = sm.OLS(y, xp).fit()
-    #run the regression
-    ypred = model.predict(xp)  #(n,) a flattened array
-    _, upper, lower = wls_prediction_std(model) # Calculate the confidence intervals
-
-    # Create scatter coordinates for best fit line
-    best_fit = go.Scatter(
-        name='Trend', 
-        x=f_df.index, 
-        y=ypred, 
-        mode='lines',
-        line=dict(color='blue', width=2)
-    )
-    #Set up the confidence intervals
-    upper_ci = go.Scatter(
-        name='Upper Confidence', 
-        x=f_df.index, 
-        y=upper, 
-        mode='lines', 
-        line=dict(color='red', width=1)
-    )
-    lower_ci = go.Scatter(
-        name='Lower Confidence', 
-        x=f_df.index, 
-        y=lower, 
-        mode='lines', 
-        line=dict(color='red', width=1)
-    )
+        mode='lines+markers')
 
     # Display the graph
-    fig = go.Figure(data=[best_fit, upper_ci, lower_ci])
+    fig = go.Figure(data=[trace])
     fig.update_layout(
-        title="Normal Gait Cycle and Deviation", 
-        xaxis_title='Percent to Completion', 
+        title="Angle during Gait Cycle", 
+        xaxis_title='Frame', 
         yaxis_title='Angle',
         showlegend=False
     )
